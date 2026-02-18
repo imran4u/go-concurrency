c779e304e9f0efd8b17c1770424d444c5bc6dc50
Imran Ali <er.imran4u@gmail.com>
2026-02-18 10:20:41 +0530

    channel

--- channel/channel_1.go
+++ channel/channel_1.go
@@ -4,6 +4,20 @@ import (
 	"fmt"
 )
 
+// func main() {
+// 	ch := make(chan int)
+
+// 	go func(a, b int) {
+// 		c := a + b
+// 		fmt.Println("c value in go routine $", c)
+// 		ch <- c
+// 	}(1, 4)
+
+// 	result, ok := <-ch
+
+// 	fmt.Printf(" On Main GR got result %d, and status %t \n", result, ok)
+// }
+
 func main() {
 	ch := make(chan int)
 
@@ -11,9 +25,16 @@ func main() {
 		c := a + b
 		fmt.Println("c value in go routine $", c)
 		ch <- c
+		ch <- 3
+		ch <- 9
+		close(ch)
 	}(1, 4)
 
 	result, ok := <-ch
 
 	fmt.Printf(" On Main GR got result %d, and status %t \n", result, ok)
+
+	for cv := range ch { // range loop will continue unless close(ch) called.
+		fmt.Printf(" cv value %d \n", cv)
+	}
 }
