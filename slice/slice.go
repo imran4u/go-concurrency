e553dd8e70080d2e5fac8bacdae4329449e47a3e
Imran Ali <er.imran4u@gmail.com>
2026-05-03 16:40:09 +0530

    sliece with new

--- slice/slice.go
+++ slice/slice.go
@@ -2,13 +2,13 @@ package main
 
 import "fmt"
 
-func main(){
-	// s := make([]int, 10)
-	s := new([]int)
-
-	fmt.Println("inital slice : ", s)
-	fmt.Println("inital size : ", len(*s))
-	fmt.Println("inital cap : ", cap(*s))
+func main() {
+	s := make([]int, 10)
+	// s := new([]int) // it is quivalent to -> var s []int
+	// so you must have to initia
+	// *s = []int{1,2,3}
+	// or
+	// *s = make([]int, 0)
 
 	for i := 0; i < 10; i++ {
 		s = append(s, i)
@@ -16,5 +16,17 @@ func main(){
 	fmt.Println("after 10 slice : ", s)
 	fmt.Println("after 10 size : ", len(s))
 	fmt.Println("after 10 cap : ", cap(s))
+
+	// with new
+
+	sp := new([]int) // it is quivalent to -> var s []int
+	// so you must have to initia
+	// *s = []int{1,2,3}
+	// or
+	// *sp = make([]int, 0)
+
+	fmt.Println("inital slice : ", sp)
+	fmt.Println("inital size : ", len(*sp))
+	fmt.Println("inital cap : ", cap(*sp))
 
 }
